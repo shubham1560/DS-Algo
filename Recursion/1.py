d572243ea0ba345ab8e8d5947ed643dc6437c729
Shubham <shubhamsinha2050@gmail.com>
2019-09-17 21:39:55 +0530

    recursion example

--- Recursion/1.py
+++ Recursion/1.py
@@ -5,4 +5,12 @@ Created on Tue Sep 17 21:29:59 2019
 @author: shubhamsinha01
 """
 
+#kickoff with the classic factorial example
 
+def factorial(n):
+    if n==0:
+        return 1
+    else:
+        return n*factorial(n-1)
+    
+    
